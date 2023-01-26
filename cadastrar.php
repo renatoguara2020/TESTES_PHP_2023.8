37e89ca12e69504593cd8094fa60a2c6082fece6
Renato Alves Soares <renatoguara2020@gmail.com>
2023-01-26 08:41:11 -0300

    OK

--- cadastrar.php
+++ cadastrar.php
@@ -1,28 +1,19 @@
-<!doctype html>
-<html lang="en">
+<?php
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-</head>
+if (isset($_POST['login'])) {
+    if (isset($_POST['username']) && ($_POST['username'] = !null)) {
+        $username = filter_input(
+            INPUT_POST,
+            'username',
+            FILTER_SANITIZE_SPECIAL_CHARS
+        );
+    }
 
-<body>
-    <h1>Hello, world!</h1>
-    <div class="container" weight="70%">
-        <form action="" method="post" enctype="multipart/form-data">
-            <label class="control-label">Username</label>
-            <input type="text" class="form-control" placeholder="Digite seu Username" name="username">
-            <label class="control-label">Password</label>
-            <input type="password" class="form-control" placeholder="Digite seu Password" name="password"><br>
-            <input type="submit" name="login" class="btn btn-primary">
-        </form>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
-    </script>
-</body>
-
-</html>
+    if (isset($_POST['password']) && ($_POST['password'] = !null)) {
+        $password = filter_input(
+            INPUT_POST,
+            'password',
+            FILTER_SANITIZE_SPECIAL_CHARS
+        );
+    }
+}
