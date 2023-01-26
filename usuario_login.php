03ee4dcb49cc3bac533644e4c66b1674b337ce67
Renato Alves Soares <renatoguara2020@gmail.com>
2023-01-26 16:33:36 -0300

    ok

---
+++ usuario_login.php
@@ -0,0 +1,22 @@
+<?php
+$usuario = $_POST['usuario'];
+$senha = $_POST['senha'];
+
+$query = "SELECT * FROM usuarios WHERE usuario='$usuario' and senha='$senha'";
+
+$conexao = new PDO('mysql:host=127.0.0.1;dbname=sitepessoal', 'root', '');
+$resultado = $conexao->query($query);
+$logado = $resultado->fetch();
+$id_logado = $logado['id'];
+
+if ($logado == null) {
+    // Usuário ou senha inválida
+    header('Location: usuario-erro.php');
+} else {
+    session_start();
+    $_SESSION['usuario_logado'] = $id_logado;
+    // Direciona o usuário para o painel administrativo do sistema
+    header('Location: painel.php');
+}
+die();
+?>
