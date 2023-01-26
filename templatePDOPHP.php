8e369ca6e7b69f3c3aa377f555a9dc7b4df964fe
Renato Alves Soares <renatoguara2020@gmail.com>
2023-01-25 21:19:28 -0300

    ok

--- templatePDOPHP.php
+++ templatePDOPHP.php
@@ -5,7 +5,15 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', '');
 define('DB_HOST', 'localhost');
 
-$conn = new PDO("mysql:host=" . DB_HOST . "; dbname=" . DB_NAME, DB_USER, DB_PASSWORD);
+$conn = new PDO(
+    'mysql:host=' . DB_HOST . '; dbname=' . DB_NAME,
+    DB_USER,
+    DB_PASSWORD
+);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-$conn->exec("set names utf8mb4");
+$conn->exec('set names utf8mb4');
+$stmt->execute([
+    'username' => $_POST['username'],
+    'password' => $_POST['password'],
+]);
